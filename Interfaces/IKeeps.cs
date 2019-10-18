c45be2f6cc9bc52041b432b20df98b64d077f196
dbaldazzi <d.baldazzi@hotmail.com>
2019-10-18 15:06:36 -0600

    added Keeps interface
    Signed-off-by: dbaldazzi <d.baldazzi@hotmail.com>

---
+++ Interfaces/IKeeps.cs
@@ -0,0 +1,20 @@
+namespace Keepr.interfaces
+{
+    public interface IKeeps
+    {
+      int id { get; set;  }
+
+      string name { get; set; }
+
+      string description { get; set; }
+
+    int userid { get; set; }
+
+      int views { get; set; }
+
+      int shares { get; set;  }
+
+      int keeps { get; set; }
+
+  }
+}
