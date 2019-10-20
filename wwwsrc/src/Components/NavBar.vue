22639427fe07a57093896d7834e2bfb68ead4eee
dbaldazzi <d.baldazzi@hotmail.com>
2019-10-20 14:01:36 -0600

    navbar built
    Signed-off-by: dbaldazzi <d.baldazzi@hotmail.com>

---
+++ wwwsrc/src/Components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="nav container-fluid d-flex"> 
+  <div class="row"> 
+    <div class="col"> 
+      <router-link to="/login" class="btn btn-secondary">Login</router-link> 
+      <router-link to="/MyVault" class="btn btn-secondary">My Vault</router-link>
+    </div> 
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "navbar", 
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}, 
+};
+</script>
+
+<style scoped> 
+.nav {
+  background: black; 
+}
+</style> 
