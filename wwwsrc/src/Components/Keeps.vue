cec5c33d1ad130cb987d5f63a5b6ef4709dc8b9e
dbaldazzi <d.baldazzi@hotmail.com>
2019-10-20 16:24:03 -0600

    keeps.vue built
    Signed-off-by: dbaldazzi <d.baldazzi@hotmail.com>

---
+++ wwwsrc/src/Components/Keeps.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="keep"> 
+  Keep Details 
+  <img :src="car.imgUrl" alt /> 
+  <h1>{{keep.name}}</h1> 
+  <h1>{{keep.description}}</h1> 
+  <span> {{keep.viewcount}} </span> 
+  <span> {{keep.shares}} </span> 
+  <span> {{keep.kept}} </span> 
+  <button class="btn btn-danger" @click="remove">Delete</button> 
+  </div> 
+</template>
+
+
+<script>
+export default {
+  name: "keep",
+  data() {
+    return {};
+  },
+  mounted() {
+    let payload = {
+      keepId: this.$route.params.keepId
+    };
+    this.$store.dispatch("getkeepById", payload); 
+  }, 
+  computed: {
+    keep() {
+      return this.$store.state.activeKeep; 
+    }
+  },
+  methods: {
+    remove() {
+      this.$store.dispatch("deleteKeep", this.keep._id); 
+  }
+  }, 
+  components: {}
+};
+</script>
+
+<style scoped>
+img {
+  height: 500px; 
+}
+</style>
