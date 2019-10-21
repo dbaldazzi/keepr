b723bbf7cc0a6d33b3f12df06f1f42e68b537c2d
dbaldazzi <d.baldazzi@hotmail.com>
2019-10-21 15:37:59 -0600

    VaultsKeeps interface and model written
    Signed-off-by: dbaldazzi <d.baldazzi@hotmail.com>

---
+++ Interfaces/IVaultKeeps.cs
@@ -0,0 +1,8 @@
+namespace keepr.Interfaces 
+{
+    public interface IVaultKeeps
+    {
+        int vaultId { get; set; }
+        int keepId { get; set;  }
+  }
+}
---
+++ Models/VaultKeeps.cs
@@ -0,0 +1,10 @@
+using keepr.Interfaces;
+
+namespace Keepr.Models
+{
+   public class VaultKeeps : IVaultKeeps
+   {
+     public int vaultId { get; set; }
+    public int keepId { get; set; }
+  }
+  } 
