107f1fc95ca289c52dcc436d5027f3fea5741fad
dbaldazzi <d.baldazzi@hotmail.com>
2019-10-20 20:14:02 -0600

    added vault.vue
    Signed-off-by: dbaldazzi <d.baldazzi@hotmail.com>

--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -3,22 +3,43 @@
     <h1>Welcome Home {{user.username}}</h1>
     <button v-if="user.id" @click="logout">logout</button>
     <router-link v-else :to="{name: 'login'}">Login</router-link>
+  <div class="container-fluid"> 
+    <div class="row">    
+  <keep v-for="keep in keeps" :keepprop="keep" :key="keep.id" />
+  <div class="col-12 pt-5"> 
+  <CreateKeepsModal /> 
+      </div>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
+import CreateKeepsModal from "../Components/CreateKeepsModal"; 
+
 export default {
   name: "home",
+  data() {
+    return {};
+  },
+  mounted() {
+    this.$store.dispatch('getKeeps');
+  },
   computed: {
     user() {
       return this.$store.state.user;
+      return this.$store.state.keeps;
     }
   },
   methods: {
     logout() {
       this.$store.dispatch("logout");
     }
+  },
+  components: {
+    CreateKeepsModal
   }
+
 };
 </script>
 
